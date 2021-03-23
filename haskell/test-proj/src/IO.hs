546a2c519631bbd1fb07037d76d3610b3c187f2c
Kussy <kushida.teppei@dcf.co.jp>
2021-03-23 18:39:00 +0900

    io

--- haskell/test-proj/src/IO.hs
+++ haskell/test-proj/src/IO.hs
@@ -2,15 +2,32 @@ module IO
     ( ioFunc
     ) where
 
+import Control.Monad
+
 ioFunc :: IO()
 
 ioFunc = do
-    line <- getLine
-    if null line
-        then return ()
-        else do
-            putStrLn $ reverseWords line
-            ioFunc
+    -- line <- getLine
+    -- if null line
+    --     then return ()
+    --     else do
+    --         putStrLn $ reverseWords line
+    --         ioFunc
+    -- colors <- forM [1, 2, 3, 4] $ \a -> do
+    --     putStrLn $ "Which color do you associate the number" ++ show a ++ "?"
+    --     getLine
+    --     -- color <- getLine
+    --     -- return color
+    -- putStrLn "The colors that you associate 1, 2, 3, and 4 are: "
+    -- mapM_ putStrLn colors
+    colors <- mapM bindcolor [1, 2, 3, 4]
+    putStrLn "The colors that you associate 1, 2, 3, and 4 are: "
+    mapM_ putStrLn colors
 
 reverseWords :: String -> String
 reverseWords = unwords . map reverse . words
+
+bindcolor :: Int -> IO String
+bindcolor a = do
+    putStrLn $ "Which color do you associate the number" ++ show a ++ "?"
+    getLine
